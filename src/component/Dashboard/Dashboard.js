247c6582bfa5ea86dc2df5a79441eaea413ab776
Eric Olaveson <olavesoneric@gmail.com>
2019-03-02 23:31:22 -0600

    dash

--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -15,3 +15,9 @@ export default Dashboard;
 //Make a list of all the different posts
 
 //Search feature and find a post
+
+//Allow someone to write a post
+
+//Allow someone to put a picture in a post
+
+//Login and logout
